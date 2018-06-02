c0c79bb5bb821ba8cd2a81b7562689c6422ab685
Pushp Abrol <pushp.abrol@gmail.com>
2018-06-01 20:19:07 -0400

    Update auth0-variables.js

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
-const AUTH0_CLIENT_ID = '<client_id>';
-const AUTH0_DOMAIN = '<domain>';
-const AUTH0_CALLBACK_URL = '<callback_url>';
+var AUTH0_CLIENT_ID = '<client_id>';
+var AUTH0_DOMAIN = '<domain>';
+var AUTH0_CALLBACK_URL = '<callback_url>';
