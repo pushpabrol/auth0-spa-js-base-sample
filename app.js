9d9d0ec4d31c2e5b07038cab41495ec0dfaa1293
Pushp Abrol <pushpabrol@MacBook-Pro-2.frontierlocal.net>
2018-10-11 09:06:56 -0400

    Adding call to API

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
 window.addEventListener('load', function() {
+
   var content = document.querySelector('.content');
   var loadingSpinner = document.getElementById('loading');
   content.style.display = 'block';
   loadingSpinner.style.display = 'none';
 
+  var userProfile;
+  var apiUrl = 'https://pse-addons.us.webtask.io/api';
+
   var webAuth = new auth0.WebAuth({
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
     redirectUri: AUTH0_CALLBACK_URL,
-    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    audience: AUTH0_AUDIENCE,
     responseType: 'token id_token',
-    scope: 'openid',
+    scope: 'openid profile read:messages',
     leeway: 60
   });
 
-  var loginStatus = document.querySelector('.container h4');
-  var loginView = document.getElementById('login-view');
   var homeView = document.getElementById('home-view');
+  var profileView = document.getElementById('profile-view');
+  var pingView = document.getElementById('ping-view');
 
   // buttons and event listeners
-  var homeViewBtn = document.getElementById('btn-home-view');
   var loginBtn = document.getElementById('qsLoginBtn');
   var logoutBtn = document.getElementById('qsLogoutBtn');
 
+  var homeViewBtn = document.getElementById('btn-home-view');
+  var profileViewBtn = document.getElementById('btn-profile-view');
+  var pingViewBtn = document.getElementById('btn-ping-view');
+
+  var pingPublic = document.getElementById('btn-ping-public');
+  var pingPrivate = document.getElementById('btn-ping-private');
+
+  var callPrivateMessage = document.getElementById('call-private-message');
+  var pingMessage = document.getElementById('ping-message');
+
+  pingPublic.addEventListener('click', function() {
+    callAPI('/', false);
+  });
+
+  pingPrivate.addEventListener('click', function() {
+    callAPI('/secured', true);
+  });
+
+  loginBtn.addEventListener('click', login);
+  logoutBtn.addEventListener('click', logout);
+
   homeViewBtn.addEventListener('click', function() {
     homeView.style.display = 'inline-block';
-    loginView.style.display = 'none';
+    profileView.style.display = 'none';
+    pingView.style.display = 'none';
   });
 
-  loginBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+  profileViewBtn.addEventListener('click', function() {
+    homeView.style.display = 'none';
+    pingView.style.display = 'none';
+    profileView.style.display = 'inline-block';
+    getProfile();
+  });
+
+  pingViewBtn.addEventListener('click', function() {
+    homeView.style.display = 'none';
+    profileView.style.display = 'none';
+    pingView.style.display = 'inline-block';
+  });
+
+  function login() {
     webAuth.authorize();
-  });
-
-  logoutBtn.addEventListener('click', logout);
+  }
 
   function setSession(authResult) {
     // Set the time that the access token will expire at
@@ -50,6 +85,7 @@ window.addEventListener('load', function() {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    pingMessage.style.display = 'none';
     displayButtons();
   }
 
@@ -58,6 +94,60 @@ window.addEventListener('load', function() {
     // access token's expiry time
     var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  function displayButtons() {
+    var loginStatus = document.querySelector('.container h4');
+    if (isAuthenticated()) {
+      loginBtn.style.display = 'none';
+      logoutBtn.style.display = 'inline-block';
+      profileViewBtn.style.display = 'inline-block';
+      pingViewBtn.style.display = 'inline-block';
+      pingPrivate.style.display = 'inline-block';
+      callPrivateMessage.style.display = 'none';
+      loginStatus.innerHTML = 'You are logged in! You can now send authenticated requests to your server.';
+    } else {
+      homeView.style.display = 'inline-block';
+      loginBtn.style.display = 'inline-block';
+      logoutBtn.style.display = 'none';
+      profileViewBtn.style.display = 'none';
+      profileView.style.display = 'none';
+      pingView.style.display = 'none';
+      pingViewBtn.style.display = 'none';
+      pingPrivate.style.display = 'none';
+      callPrivateMessage.style.display = 'block';
+      loginStatus.innerHTML = 'You are not logged in! Please log in to continue.';
+    }
+  }
+
+  function getProfile() {
+    if (!userProfile) {
+      var accessToken = localStorage.getItem('access_token');
+
+      if (!accessToken) {
+        console.log('Access token must exist to fetch profile');
+      }
+
+      webAuth.client.userInfo(accessToken, function(err, profile) {
+        if (profile) {
+          userProfile = profile;
+          displayProfile();
+        }
+      });
+    } else {
+      displayProfile();
+    }
+  }
+
+  function displayProfile() {
+    // display the profile
+    document.querySelector(
+      '#profile-view .nickname'
+    ).innerHTML = userProfile.nickname;
+    document.querySelector(
+      '#profile-view .full-profile'
+    ).innerHTML = JSON.stringify(userProfile, null, 2);
+    document.querySelector('#profile-view img').src = userProfile.picture;
   }
 
   function handleAuthentication() {
@@ -78,18 +168,30 @@ window.addEventListener('load', function() {
     });
   }
 
-  function displayButtons() {
-    if (isAuthenticated()) {
-      loginBtn.style.display = 'none';
-      logoutBtn.style.display = 'inline-block';
-      loginStatus.innerHTML = 'You are logged in!';
-    } else {
-      loginBtn.style.display = 'inline-block';
-      logoutBtn.style.display = 'none';
-      loginStatus.innerHTML =
-        'You are not logged in! Please log in to continue.';
+  handleAuthentication();
+
+  function callAPI(endpoint, secured) {
+    var url = apiUrl + endpoint;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    if (secured) {
+      xhr.setRequestHeader(
+        'Authorization',
+        'Bearer ' + localStorage.getItem('access_token')
+      );
     }
+    xhr.onload = function() {
+      if (xhr.status == 200) {
+        // update message
+        document.querySelector('#ping-view h2').innerHTML = JSON.parse(
+          xhr.responseText
+        ).message;
+      } else {
+        alert('Request failed: ' + xhr.statusText);
+      }
+    };
+    xhr.send();
   }
 
-  handleAuthentication();
+  displayButtons();
 });
